83208c2f0915370ff0ea11a1ff4c3a44eeb2372e
Jimmy Rozo <jimmyrozo29@gmail.com>
2024-04-01 20:59:03 -0500

    fixed | changes pageProps

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,9 +6,9 @@ import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
-    <SessionProvider session={pageProps.session}>
+    <SessionProvider session={session}>
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
           <Component {...pageProps} />
