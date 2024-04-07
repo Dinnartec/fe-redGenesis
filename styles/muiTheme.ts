8db3c665865189e644a749ed4f305c3c4f46a7ea
Jimmy Rozo <jimmyrozo29@gmail.com>
2024-04-06 21:12:00 -0500

    Implementation sidebar and menu actions

---
+++ styles/muiTheme.ts
@@ -0,0 +1,13 @@
+import { createTheme } from '@mui/material';
+
+export const redGenesisTheme = createTheme({
+  palette: {
+    mode: 'light',
+    primary: {
+      main: '#1D5C63',
+    },
+    secondary: {
+      main: '#2C98A0',
+    },
+  },
+});
